a2055690eb7b81deb709c62e1cb65ed4321e3752
Hariprathap Interloid <hariprathap@interloid.com>
2026-06-26 18:55:13 +0530

    completed the header desgin

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,12 +1,16 @@
-import {Suspense} from 'react';
-import {Await, NavLink, useAsyncValue} from 'react-router';
+import { Suspense } from 'react';
+import { Await, NavLink, useAsyncValue } from 'react-router';
 import {
   type CartViewPayload,
   useAnalytics,
   useOptimisticCart,
 } from '@shopify/hydrogen';
-import type {HeaderQuery, CartApiQueryFragment} from 'storefrontapi.generated';
-import {useAside} from '~/components/Aside';
+import type { HeaderQuery, CartApiQueryFragment } from 'storefrontapi.generated';
+import { useAside } from '~/components/Aside';
+import SearchIcon from './icons/header/search';
+import CartIcon from './icons/header/cart';
+import MenuIcon from './icons/header/menu';
+import UserIcon from './icons/header/user';
 
 interface HeaderProps {
   header: HeaderQuery;
@@ -23,19 +27,22 @@ export function Header({
   cart,
   publicStoreDomain,
 }: HeaderProps) {
-  const {shop, menu} = header;
+  const { shop, menu } = header;
   return (
-    <header className="header">
-      <NavLink prefetch="intent" to="/" style={activeLinkStyle} end>
-        <strong className="font-quantify">{shop.name}</strong>
-      </NavLink>
-      <HeaderMenu
-        menu={menu}
-        viewport="desktop"
-        primaryDomainUrl={header.shop.primaryDomain.url}
-        publicStoreDomain={publicStoreDomain}
-      />
-      <HeaderCtas isLoggedIn={isLoggedIn} cart={cart} />
+    <header className="header top-3 flex max-h-11.25 tems-center gap-4 w-full lg:max-h-16.25 lg:top-5.5">
+      <div className="mx-auto flex h-full w-full max-w-7xl 2xl:max-w-375 items-center gap-4 rounded-full bg-header-bg px-6 py-3 box-border lg:px-11.25">
+        <HeaderMenuMobileToggle />
+        <NavLink prefetch="intent" to="/" className="hover:no-underline!" end>
+          <span className="text-[26px] tracking-normal leading-15.5 text-heading font-quantify font-normal align-middle  xl:text-[40px]">Elora.</span>
+        </NavLink>
+        <HeaderMenu
+          menu={menu}
+          viewport="desktop"
+          primaryDomainUrl={header.shop.primaryDomain.url}
+          publicStoreDomain={publicStoreDomain}
+        />
+        <HeaderCtas isLoggedIn={isLoggedIn} cart={cart} />
+      </div>
     </header>
   );
 }
@@ -51,8 +58,8 @@ export function HeaderMenu({
   viewport: Viewport;
   publicStoreDomain: HeaderProps['publicStoreDomain'];
 }) {
-  const className = `header-menu-${viewport}`;
-  const {close} = useAside();
+  const className = `header-menu-${viewport} flex  justify-center w-full`;
+  const { close } = useAside();
 
   return (
     <nav className={className} role="navigation">
@@ -73,13 +80,13 @@ export function HeaderMenu({
         // if the url is internal, we strip the domain
         const url =
           item.url.includes('myshopify.com') ||
-          item.url.includes(publicStoreDomain) ||
-          item.url.includes(primaryDomainUrl)
+            item.url.includes(publicStoreDomain) ||
+            item.url.includes(primaryDomainUrl)
             ? new URL(item.url).pathname
             : item.url;
         return (
           <NavLink
-            className="header-menu-item"
+            className="header-menu-item font-inter font-medium tracking-[15%] align-bottom leading-[130%] hover:decoration-0 underline-offset-12 decoration-2! decoration-primary!"
             end
             key={item.id}
             onClick={close}
@@ -100,45 +107,44 @@ function HeaderCtas({
   cart,
 }: Pick<HeaderProps, 'isLoggedIn' | 'cart'>) {
   return (
-    <nav className="header-ctas" role="navigation">
-      <HeaderMenuMobileToggle />
-      <NavLink prefetch="intent" to="/account" style={activeLinkStyle}>
+    <nav className="header-ctas pr-px" role="navigation">
+      <SearchToggle />
+      <NavLink prefetch="intent" to="/account" style={activeLinkStyle} className={"hidden lg:block h-fit w-fit"}>
         <Suspense fallback="Sign in">
           <Await resolve={isLoggedIn} errorElement="Sign in">
-            {(isLoggedIn) => (isLoggedIn ? 'Account' : 'Sign in')}
+            {(isLoggedIn) => (isLoggedIn ? 'Account' : <UserIcon/>)}
           </Await>
         </Suspense>
       </NavLink>
-      <SearchToggle />
       <CartToggle cart={cart} />
     </nav>
   );
 }
 
 function HeaderMenuMobileToggle() {
-  const {open} = useAside();
+  const { open } = useAside();
   return (
     <button
-      className="header-menu-mobile-toggle reset"
+      className="header-menu-mobile-toggle reset bg-header-bg"
       onClick={() => open('mobile')}
     >
-      <h3>☰</h3>
+      <MenuIcon />
     </button>
   );
 }
 
 function SearchToggle() {
-  const {open} = useAside();
+  const { open } = useAside();
   return (
     <button className="reset" onClick={() => open('search')}>
-      Search
+      <SearchIcon />
     </button>
   );
 }
 
-function CartBadge({count}: {count: number}) {
-  const {open} = useAside();
-  const {publish, shop, cart, prevCart} = useAnalytics();
+function CartBadge({ count }: { count: number }) {
+  const { open } = useAside();
+  const { publish, shop, cart, prevCart } = useAnalytics();
 
   return (
     <a
@@ -154,12 +160,13 @@ function CartBadge({count}: {count: number}) {
         } as CartViewPayload);
       }}
     >
-      Cart <span aria-label={`(items: ${count})`}>{count}</span>
+      <CartIcon />
+      {/* <span aria-label={`(items: ${count})`}>{count}</span> */}
     </a>
   );
 }
 
-function CartToggle({cart}: Pick<HeaderProps, 'cart'>) {
+function CartToggle({ cart }: Pick<HeaderProps, 'cart'>) {
   return (
     <Suspense fallback={<CartBadge count={0} />}>
       <Await resolve={cart}>
---
+++ app/components/icons/header/menu.tsx
@@ -0,0 +1,19 @@
+export default function MenuIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="12"
+      viewBox="0 0 16 12"
+      fill="none"
+    >
+      <path
+        d="M1 1H14.3333M1 6H7.1875M1 11H12.1875"
+        stroke="#222222"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
